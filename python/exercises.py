dca6914646dc5a34f19ab1cc35b5978ef70c2357
Cameron Scolari <cameronscolari@outlook.com>
2024-08-28 23:11:18 -0700

    started 3 python solutions. say() function (problem 3) i don't think follows proper instructions. review later and fix syntax, add comments, fix program efficiency, etc.

--- python/exercises.py
+++ python/exercises.py
@@ -14,15 +14,83 @@ def change(amount: int) -> dict[int, int]:
 
 
 # Write your first then lower case function here
-
+def first_then_lower_case(a, p):
+    '''
+    
+    '''
+    for string in a:
+        if p(string):
+            return string.lower()
+    return None
 
 # Write your powers generator here
-
+def powers_generator(base, limit):
+    '''
+    
+    '''
+    number = 0
+    while base**number <= limit:
+        yield base**number
+        number += 1
 
 # Write your say function here
-
+def say(word=""):
+    '''
+    
+    '''
+    def next_word(next=None):
+        if next is None:
+            return next_word.word
+        next_word.word += f" {next}"
+        return next_word
+    next_word.word = word
+    return next_word
 
 # Write your line count function here
+def meaningful_line_count(file):
+    '''
+    Definition
 
+    Parameters:
+
+    Returns:
+
+    Raises: 
+    '''
+    try:
+        with open(file, "r", encoding="utf-8") as f:
+            line_counter = 0
+            for line in f:
+                line = line.strip()
+                if not line or line.startswith("#"):
+                    continue
+                line_counter += 1
+            return line_counter
+    except FileNotFoundError:
+        raise FileNotFoundError("No such file")\
 
 # Write your Quaternion class here
+class Quarternion():
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b + "i"
+        self.c = c + "j"
+        self.d = d + "k"
+    
+    def __str__(self):
+        return
+    
+
+    def add():
+        return 
+    
+
+    def multiply():
+        return
+    
+    
+    def coefficients():
+        return
+    
+    def conjugate():
+        return
