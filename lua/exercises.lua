93638e8e6c2af381a90e2f3cf8e185bf86370756
Atul Venkatesan <atulvenky@gmail.com>
2024-08-30 17:38:08 -0700

    worked on Lua exercises 1 and 2 and partially 3

--- lua/exercises.lua
+++ lua/exercises.lua
@@ -14,10 +14,42 @@ function change(amount)
 end
 
 -- Write your first then lower case function here
+  function first_then_lower_case(a, p)
+    for i in a do
+      if p(i) then
+        return string.lower(i)        
+      end
+    end
+    return nil
+  end
 
 -- Write your powers generator here
+function powers_generator(base, limit)
+  local total = 1
+  -- create coroutine
+  return coroutine.create(function()
+    while total <= limit do
+      -- pause coroutine and return total
+      coroutine.yield(total)
+      total = total * base
+    end
+  -- need to close coroutine with parenthesis
+  end)
+end
 
 -- Write your say function here
+  function say(sentence)
+    local words = {}
+    -- if there is a sentence provided, add to table, else add the empty quotes
+    if sentence then
+      table.insert(words, sentence)
+      return words
+    else
+      return ""
+    end
+    -- will need to create a helper method that can be called for each string passed and add a empty space at the end when concatenating
+
+  end
 
 -- Write your line count function here
 
