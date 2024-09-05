6c7f0c65f9d6ae03f757945bc8309666bb1f575c
Atul Venkatesan <atulvenky@gmail.com>
2024-09-05 16:52:20 -0700

    worked on lua exercise 3 and still in progress

--- lua/exercises.lua
+++ lua/exercises.lua
@@ -15,7 +15,7 @@ end
 
 -- Write your first then lower case function here
   function first_then_lower_case(a, p)
-    for i in a do
+    for _, i in ipairs(a) do
       if p(i) then
         return string.lower(i)        
       end
@@ -40,17 +40,16 @@ end
 -- Write your say function here
   function say(sentence)
     local words = {}
-    -- if there is a sentence provided, add to table, else add the empty quotes
     if sentence then
       table.insert(words, sentence)
-      return words
-    else
-      return ""
     end
-    -- will need to create a helper method that can be called for each string passed and add a empty space at the end when concatenating
+    local output
+
+    for 
 
   end
 
 -- Write your line count function here
+  --function meaningful_line_count(file)
 
 -- Write your Quaternion table here
