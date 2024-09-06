650aaa9b32b1451afb06864f84e9a5bb8079c512
Cameron Scolari <cameronscolari@outlook.com>
2024-09-05 17:54:26 -0700

    finished python exercises. style should be checked but exercises should be implemented using proper python practice.

--- python/exercises.py
+++ python/exercises.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from collections.abc import Callable
+from collections.abc import Callable, Generator
 
 
 def change(amount: int) -> dict[int, int]:
@@ -14,185 +14,148 @@ def change(amount: int) -> dict[int, int]:
 
 
 # Write your first then lower case function here
-def first_then_lower_case(a, p):
-    '''
-    
-    '''
+def first_then_lower_case(
+        a: list[str],
+        p: Callable[[str], bool]
+) -> str | None:
     for string in a:
         if p(string):
             return string.lower()
     return None
 
+
 # Write your powers generator here
-def powers_generator(base, limit):
-    '''
-    
-    '''
-    number = 0
-    while base**number <= limit:
-        yield base**number
-        number += 1
+def powers_generator(*, base: int, limit: int) -> Generator[int, None, None]:
+    exponent = 0
+    while base**exponent <= limit:
+        yield base**exponent
+        exponent += 1
+
 
 # Write your say function here
-def say(word=""):
-    '''
-    
-    '''
-    def next_word(next=None):
+def say(word: str | None = None, /) -> str:
+    if word is None:
+        return ''
+    def next_word(next: str | None = None) -> 'next_word':
         if next is None:
             return next_word.word
-        next_word.word += f" {next}"
+        next_word.word += f' {next}'
         return next_word
     next_word.word = word
     return next_word
 
+
 # Write your line count function here
-def meaningful_line_count(file):
-    '''
-    Definition
-
-    Parameters:
-
-    Returns:
-
-    Raises: 
-    '''
+def meaningful_line_count(file_name: str, /) -> int:
     try:
-        with open(file, "r", encoding="utf-8") as f:
-            line_counter = 0
-            for line in f:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            meaningful_line_counter = 0
+            for line in file:
                 line = line.strip()
-                if not line or line.startswith("#"):
+                if not line or line.startswith('#'):
                     continue
-                line_counter += 1
-            return line_counter
+                meaningful_line_counter += 1
+            return meaningful_line_counter
     except FileNotFoundError:
-        raise FileNotFoundError("No such file")
+        raise FileNotFoundError('No such file')
+
 
 # Write your Quaternion class here
+@dataclass(frozen=True)
 class Quaternion():
-    def __init__(self, a, b, c, d):
-        self.a = float(a)
-        self.b = float(b)
-        self.c = float(c)
-        self.d = float(d)
+    a: float
+    b: float
+    c: float
+    d: float
     
-    def __eq__(self, other):
+    def __eq__(self, other: 'Quaternion', /) -> bool:
         if isinstance(other, Quaternion):
-            if self.a == other.a:
-                pass
-            else:
+            if self.a != other.a:
                 return False
-            if self.b == other.b:
-                pass
-            else:
+            if self.b != other.b:
                 return False
-            if self.c == other.c:
-                pass
-            else:
+            if self.c != other.c:
                 return False
-            if self.d == other.d:
-                pass
-            else:
+            if self.d != other.d:
                 return False
             return True
     
     def __str__(self):
-        local_a = str(self.a)
-        local_b = str(self.b)
-        local_c = str(self.c)
-        local_d = str(self.d)
-        if local_a == "0.0":
-            local_a = ""
-        if local_b == "0.0" or local_b == "-0.0":
-            local_b = ""
-        if local_c == "0.0" or local_c == "-0.0":
-            local_c = ""
-        if local_d == "0.0" or local_d == "-0.0":
-            local_d = ""
+        a_string = str(float(self.a))
+        b_string = str(float(self.b)) + 'i'
+        c_string = str(float(self.c)) + 'j'
+        d_string = str(float(self.d)) + 'k'
 
-        if local_b != "":
-            if local_b == "1.0" or local_b == "-1.0":
-                if local_b == "-1.0":
-                    local_b = "-"
-                else:
-                    local_b = ""
-            local_b = local_b + "i"
-        if local_c != "":
-            if local_c == "1.0" or local_c == "-1.0":
-                if local_c == "-1.0":
-                    local_c = "-"
-                else:
-                    local_c = ""
-            local_c = local_c + "j"
-        if local_d != "":
-            if local_d == "1.0" or local_d == "-1.0":
-                if local_d == "-1.0":
-                    local_d = "-"
-                else:
-                    local_d = ""
-            local_d = local_d + "k"
+        if a_string == '0.0':
+            a_string = ''
+
+        match b_string:
+            case '0.0i':
+                b_string = ''
+            case '1.0i':
+                b_string = 'i'
+            case '-1.0i':
+                b_string = '-i'
+
+        match c_string:
+            case '0.0j':
+                c_string = ''
+            case '1.0j':
+                c_string = 'j'
+            case '-1.0j':
+                c_string = '-j'
         
-        # needs to check if b is "", if that is, then check c, if that is then check d. if all are "" then pass.
-
-        if local_b == "":
-            if local_c == "":
-                if local_d == "":
-                    pass
-                else:
-                    if local_d[0] != "-" and local_a != "":
-                        local_a = local_a + "+"
-            else:
-                if local_c[0] != "-" and local_a != "":
-                    local_a = local_a + "+"
-        else:
-            if local_b[0] != "-" and local_a != "":
-                local_a = local_a + "+"
-
-        # if local_b == "" and local_c == "" and local_d == "":
-        #     pass
-        # else:
-        #     if local_b[0] != "-" and local_a != "":
-        #         local_a = local_a + "+"
-        if local_c == "":
-            if local_d == "":
-                pass
-            else:
-                if local_d[0] != "-" and local_a != "":
-                    local_a = local_a + "+"
-        else:
-            if local_c[0] != "-" and local_a != "":
-                local_a = local_a + "+"
-        if local_d == "":
-            pass
-        else:
-            if local_d[0] != "-" and local_c != "":
-                local_c += "+"
+        match d_string:
+            case '0.0k':
+                d_string = ''
+            case '1.0k':
+                d_string = 'k'
+            case '-1.0k':
+                d_string = '-k'
         
-        print(local_a + local_b + local_c + local_d)
+        return_str = a_string + '+' + b_string + '+' + c_string + '+' + d_string
 
-        return local_a + local_b + local_c + local_d
+        return_str = return_str.replace('++', '+')
+        return_str = return_str.replace('+-', '-')
 
-    def __add__(self, other):
+        if return_str == '':
+            return '0'
+
+        if return_str[len(return_str) - 1] == '+':
+            return_str = return_str[0:len(return_str) - 1]
+        
+        if return_str[0] == '+':
+            return_str = return_str[1:]        
+
+        if return_str == '':
+            return '0'
+        
+        return return_str
+
+    def __add__(self, other: 'Quaternion', /) -> 'Quaternion':
         if isinstance(other, Quaternion):
-            new_a = self.a + other.a
-            new_b = self.b + other.b
-            new_c = self.c + other.c
-            new_d = self.d + other.d
-            return Quaternion(new_a, new_b, new_c, new_d)
+            a_sum = self.a + other.a
+            b_sum = self.b + other.b
+            c_sum = self.c + other.c
+            d_sum = self.d + other.d
+            return Quaternion(a_sum, b_sum, c_sum, d_sum)
 
-    def __mul__(self, other):
+    def __mul__(self, other: 'Quaternion', /) -> 'Quaternion':
         if isinstance(other, Quaternion):
-            new_a = (self.a * other.a) - (self.b * other.b) - (self.c * other.c) - (self.d * other.d)
-            new_b = (self.a * other.b) + (self.b * other.a) + (self.c * other.d) - (self.d * other.c)
-            new_c = (self.a * other.c) - (self.b * other.d) + (self.c * other.a) + (self.d * other.b)
-            new_d = (self.a * other.d) + (self.b * other.c) - (self.c * other.b) + (self.d * other.a)
-            return Quaternion(new_a, new_b, new_c, new_d)
+            a_prod = (self.a * other.a) - (self.b * other.b) \
+                    - (self.c * other.c) - (self.d * other.d)
+            b_prod = (self.a * other.b) + (self.b * other.a) \
+                    + (self.c * other.d) - (self.d * other.c)
+            c_prod = (self.a * other.c) - (self.b * other.d) \
+                    + (self.c * other.a) + (self.d * other.b)
+            d_prod = (self.a * other.d) + (self.b * other.c) \
+                    - (self.c * other.b) + (self.d * other.a)
+            return Quaternion(a_prod, b_prod, c_prod, d_prod)
     
     @property
-    def coefficients(self):
+    def coefficients(self) -> tuple[float]:
         return self.a, self.b, self.c, self.d
     
     @property
-    def conjugate(self):
+    def conjugate(self) -> 'Quaternion':
         return Quaternion(self.a, self.b * -1, self.c * -1, self.d * -1)
