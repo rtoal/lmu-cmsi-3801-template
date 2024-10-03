c00ccb129d3223dadf922e0dbb1f4741befe8314
Cameron Scolari <cameronscolari@outlook.com>
2024-10-02 17:59:18 -0700

    Removed 1 unused import

--- java/Exercises.java
+++ java/Exercises.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.stream.Stream;
 import java.util.ArrayList;
 
 public class Exercises {
